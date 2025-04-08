a97cd416b9f1e820c4d54ea364e167e146a3782a
Lukyanenko Kate <p4eeeeelka@gmail.com>
2025-04-08 22:44:19 +0300

    Initial commit

---
+++ lazywallet/src/main/java/com/lazywallet/lazywallet/LazyWalletApplication.java
@@ -0,0 +1,14 @@
+package com.lazywallet.lazywallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyWalletApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LazyWalletApplication.class, args);
+	}
+
+}
---
+++ lazywallet/src/test/java/com/lazywallet/lazywallet/LazyWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazywallet.lazywallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
